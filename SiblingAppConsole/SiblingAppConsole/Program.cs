c32f3ec05a83a72b3d8525e4ff5492549633c6ba
Adriano Santos <adrianosanttos1987@hotmail.com>
2016-06-29 22:53:57 -0300

    C#: Retornar Maior Número Família
    Dois números inteiros não negativos são chamados de irmãos se eles podem
    ser obtidos a partir de cada outra
    rearranjando os dígitos de suas representações decimais. Por exemplo,
    123 e 213
    são irmãos. 535 e 355 são também irmãos.
    Um conjunto constituído por um grupo N número inteiro não negativo e
    todos os seus irmãos é chamado a família de
    N. Por exemplo, a família de 553 compreende três números: 355, 535 e
    553.
    
    Escrever uma função:
    
    solução int (int N);
    
    que, dado um número inteiro não negativo N, devolve o maior número na
    família de N. A
    função deve retornar -1 se o resultado for superior a 100 milhões.
    
    Por exemplo, dado N = 213 a função deve retornar 321. Dado N = 553 da
    função
    
    deve retornar 553.
    
    Two non-negative integers are called siblings if they can be obtained
    from each other
    by rearranging the digits of their decimal representations. For example,
    123 and 213
    are siblings. 535 and 355 are also siblings.
    A set consisting of a non-negative integer N and all of its siblings is
    called the family of
    N. For example, the family of 553 comprises three numbers: 355, 535 and
    553.
    
    Write a function:
    
    int solution(int N);
    
    that, given a non-negative integer N, returns the largest number in the
    family of N. The
    function should return −1 if the result exceeds 100,000,000.
    
    For example, given N = 213 the function should return 321. Given N = 553
    the function
    
    should return 553.

---
+++ SiblingAppConsole/SiblingAppConsole/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SiblingAppConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            //O número irmão é solicitado
+            Console.WriteLine("Enter a number:", num);
+            num = Convert.ToInt32(Console.ReadLine());
+            
+            int returnMain = Solution(num);
+            //O Console mostra o maior numero da familia
+            Console.WriteLine("Biggest value of the number family: {0}", returnMain);
+        }
+
+        static private int Solution(int N)
+        {
+            int nReturn = 0;
+            //O número digitado é armazenado dentro do Array
+            int[] eachNumber = N.ToString().ToCharArray().Select(x => (int)Char.GetNumericValue(x)).ToArray();
+            //O Array é ordenado de forma decrescente, gerando assim o maior numero 
+            string strBiggestValue = string.Join("", eachNumber.OrderByDescending(x => x));
+
+            //Se o resultado for superior a 100 milhões, retorna -1
+            if (Convert.ToInt32(strBiggestValue) > 100000000)
+                nReturn = -1;
+            else
+                nReturn = Convert.ToInt32(strBiggestValue);
+
+            return nReturn;
+        }
+    }
+}
